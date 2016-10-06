8725c027bd5a5cee2d2745e702f69431920463c7
shafdanny <shafdanny@gmail.com>
2016-10-06 11:12:27 +0200

    Initial commit

---
+++ app/src/main/java/com/shafiqdaniel/fisheye/PolygonView.java
@@ -0,0 +1,21 @@
+package com.shafiqdaniel.fisheye;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by user on 06/10/2016.
+ */
+
+public class PolygonView extends View {
+    public PolygonView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
